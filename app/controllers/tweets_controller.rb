8d88fd21e622d7ec7fa51eb35d7b7230242c5375
kawashinshin <kawakamis@kawakamishintarou-no-MacBook-Pro.local>
2016-05-23 19:07:55 +0900

    Revert "Revert "問題１解決""
    This reverts commit 674ccff4016f0da8f8158a31cabf9846a9207250.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
-  before_action :move_to_index, except :index
+
+  before_action :move_to_index, except: :index
 
   def index
     @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
