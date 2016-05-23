5fd3a7f4f1fcc5b64b16841bd3d8d5ec136747be
kawashinshin <kawakamis@kawakamishintarou-no-MacBook-Pro.local>
2016-05-23 21:07:06 +0900

    問題７解決

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 devise_for :users
   root 'tweets#index'                        #
 
-  post  'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
+  get  'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
   post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
   delete 'tweets/:id' =>  'tweets#destroy'   #ツイート削除機能
   get   'tweets/:id/edit'  =>  'tweets#edit' #ツイート編集画面
